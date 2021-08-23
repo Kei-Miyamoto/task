dbebe0e73b3267a74f3c55412fe3eafdd65a83c3
Kei Miyamoto <k.miyamoto95@gmail.com>
2021-08-23 19:00:37 +0900

    編集機能できるか不明

--- app/Http/Controllers/Detail_EditController.php
+++ app/Http/Controllers/Detail_EditController.php
@@ -49,4 +49,38 @@ class Detail_EditController extends Controller
     
     return view('edit', ['product_edit' => $product_edit],['company' => $company]);
   }
+
+
+    /**
+     * 商品情報を更新する
+     * @return view
+     */
+    public function exeUpdate(PostRequest $request) {
+      \DB::beginTransaction();
+      try {
+        $inputs = $request->all();
+        $product_edit = Product::find($inputs['id']);
+        $companyId = DB::table('products')->select('company_id')->leftJoin('companies', 'products.company_id', '=', 'companies.id')
+          ->where('company_name', '=', $request['company_name'])
+          ->get();
+        $company_id = $companyId[0]->company_id;
+
+        $inputs = [
+          $product_edit->product_name = $request->product_name,
+          $product_edit->company_id =  $company_id,
+          $product_edit->price = $request->price,
+          $product_edit->stock = $request->stock,
+          $product_edit->comment = $request->comment
+        ];
+        $product_edit->fill($inputs)->save();
+
+        \DB::commit();
+        \Session::flash('msg_success', '商品情報を更新しました');
+      } catch (\Throwable $e){
+        \DB::rollback();
+        abort(500);
+        \Session::flash('msg_error', '商品情報を更に失敗しました');
+      } 
+      return redirect(route('home'));
+    }
 }
