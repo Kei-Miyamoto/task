dffc30441ac7d934a6a1f847896025f110ab8d73
Kei Miyamoto <k.miyamoto95@gmail.com>
2021-08-29 18:01:55 +0900

    ログインフォーム

--- resources/views/loginForm.blade.php
+++ resources/views/loginForm.blade.php
@@ -5,54 +5,48 @@
 <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
 
 <body>
-  <div class="row justify-content-center">
-    <script>
-      @if (Session::has('msg_success'))
-        $(function() {
-          toastr.success('{{ session('msg_success') }}');
-        });
-      @endif
-    </script>
-    <script>
-      @if (Session::has('msg_error'))
-        $(function() {
-          toastr.error('{{ session('msg_error') }}');
-        });
-      @endif
-    </script>
-
-    <div class="col-md-8">
-      <br>
+  <script>
+    @if (Session::has('msg_success'))
+      $(function() {
+        toastr.success('{{ session('msg_success') }}');
+      });
+    @endif
+  </script>
+  <script>
+    @if (Session::has('msg_error'))
+      $(function() {
+        toastr.error('{{ session('msg_error') }}');
+      });
+    @endif
+  </script>
+  <div class="auth-wrapper">
+    <div class="container auth-container">
       <div class="card">
-        <form class="" method="POST" action="{{ route('login') }}">
-          @csrf
-          <h5 class="card-header text-center">ログイン画面</h1>
-
-          @if ($errors->any())
-            <div class="alert alert-danger">
-              <ul>
-                @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-                @endforeach
-              </ul>
-            </div>
-          @endif
-          <div class="card-body">
+        <h5 class="card-header text-center">ログイン画面</h5>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+        @endif
+        
+        <div class="card-body">
+          <form class="" method="POST" action="{{ route('login') }}">
+            @csrf
             <div class="form-group row">
-              <label class="col-md-4 col-form-label text-md-right" for="">メールアドレス</label>
-              <div class="col-md-7">
-                <input type="email" class="form-control" id="email" name="email" placeholder="" required>
-              </div>
+              <label class="col-sm-12 col-md-4 col-lg-4 col-form-label text-md-center" for="">メールアドレス</label>
+              <input type="email" class="form-control col-sm-12 col-md-8 col-lg-8" id="email" name="email" placeholder="" required>
             </div>
             <br>
-
-            <div class="form-group row boxxx">
-              <label class="col-md-4 col-form-label text-md-right" for="">パスワード</label>
-              <div class="col-md-7 box-pass">
-                <input type="password" class="form-control js-password" id="password" name="password" placeholder="" required>
-                <input class="js-password-toggle button__input hidden-box" id="password--eye" type="checkbox" name="password-eye">
-                <label class="js-password-label button__label" for="password--eye"><i class="fas fa-eye"></i></label>
-              </div>
+  
+            <div class="form-group row pass-box">
+              <label class="col-md-4 col-form-label text-md-center" for="">パスワード</label>
+              <input type="password" class="form-control js-password form-control col-sm-12 col-md-8 col-lg-8" id="password" name="password" placeholder="" required>
+              <input class="js-password-toggle button__input hidden-box " id="password--eye" type="checkbox" name="password-eye">
+              <label class="js-password-label button__label" for="password--eye"><i class="fas fa-eye"></i></label>
             </div>
             <br>
           
@@ -64,6 +58,7 @@
         </form>
       </div>
     </div>
+  
 </div>
 <!-- パスワード非表示/表示 -->
 <script></script>
